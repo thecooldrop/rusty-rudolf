e5ab4e6e9313ba59a2f3309c9045ff7eccd97e9c
Vanio Begic <vanio.begic123@gmail.com>
2021-03-09 21:47:17 +0100

    Improve documentation for module structure

--- src/filter/kalman/mod.rs
+++ src/filter/kalman/mod.rs
@@ -1,2 +1,8 @@
+//! Contains modules representing algorithms based on Kalman filtering methodology. Currently
+//! the algorithms are classified into the following modules:
+//! - Linear algorithms ; based on regular kalman filter
+//! - nonlinear algorithms; based on extended Kalman filter ( linear approximations ),
+//!   unscented ( cubature methods ) and others.
+
 pub mod linear;
 pub mod nonlinear;
--- src/filter/kalman/nonlinear/mod.rs
+++ src/filter/kalman/nonlinear/mod.rs
@@ -1,1 +1,3 @@
+//! Algorithms containing non-linear approximations to optimal Kalman filter.
+
 pub mod extended;
